fa75420415bd4f45ec13172e322f9f5c04397948
Kyle Claravall <62874933+kyleclaravall@users.noreply.github.com>
2020-11-03 12:24:15 +1100

    Update step03_primerCSV.py

--- dataWrangling/step03_primerCSV.py
+++ dataWrangling/step03_primerCSV.py
@@ -1,17 +1,20 @@
 # step03_primerCSV.py
 
 import pandas as pd
-pd.set_option("display.max_rows", 3, "display.max_columns", None)
 
+# change viewing options
+pd.set_option("display.max_rows", 3, "display.max_columns", None)
 
 # read in subsetData
 subsetData = pd.read_csv('subsetData.csv', sep=',', header=0)
-
-
 primerCSV = subsetData.copy()
+
+# drop metaData columns, retain the species data
 primerCSV = primerCSV.drop(['ID', 'Plot', 'Burn', 'Dune'], axis=1)
-primerCSV.to_csv(r'/Users/kyleclaravall/Google Drive/UNI DOCS/2020_SEM2/BIOL3907/Seed Banks/local_panda/primerCSV.csv', index=False)
+# write out file
+primerCSV.to_csv(r'/path/to/primerCSV.csv', index=False)
 
-
+# drop species data, retain the metaData columns
 siteFactors = subsetData[['ID', 'Plot', 'Burn', 'Dune']]
-siteFactors.to_csv(r'/Users/kyleclaravall/Google Drive/UNI DOCS/2020_SEM2/BIOL3907/Seed Banks/local_panda/siteFactors.csv', index=False)
+# write out file
+siteFactors.to_csv(r'/path/to/siteFactors.csv', index=False)
